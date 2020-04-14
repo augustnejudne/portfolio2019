0c5d15122334a0aab262bf1d5d03e4f257060e7d
devaugustnejudne <dev.august.nejudne@gmail.com>
2020-04-14 08:41:47 +0800

    fixed typo

--- src/components/about-me/index.js
+++ src/components/about-me/index.js
@@ -8,7 +8,7 @@ const AboutMe = props => {
     <div className={classes.container}>
       <img src={myPic} alt="me" />
       <p>I'm a full-stack javascript developer.</p>
-      <p>I love programming, philosopy, and ping-pong.</p>
+      <p>I love programming, philosophy, and ping-pong.</p>
       <a href={resume}>Download my resume</a>
       <a href="mailto:work.august.nejudne@gmail.com"><span>Email me:</span> work.august.nejudne@gmail.com</a>
     </div>
