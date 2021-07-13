c15e7758ac035b6275d4483acbdd4e1103d5ea60
augustnejudne <august.nejudne@gmail.com>
2021-07-14 03:20:43 +0800

    change reference to resume pdf

--- src/components/about-me/index.js
+++ src/components/about-me/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import classes from "./styles.module.scss";
 import myPic from "../../assets/my-pic.jpeg";
-import resume from "../../assets/Kim_Nejudne_Resume_Apr_2020.pdf";
+import resume from "../../assets/Kim_Nejudne_Resume_July_2021.pdf";
 
 const AboutMe = props => {
   return (
@@ -9,7 +9,7 @@ const AboutMe = props => {
       <img src={myPic} alt="me" />
       <p>I'm a full-stack javascript developer.</p>
       <p>I love programming, philosophy, and ping-pong.</p>
-      <a href={resume}>Download my resume</a>
+      <a href={resume} target="blank">Download my resume</a>
       <a href="mailto:work.august.nejudne@gmail.com"><span>Email me:</span> work.august.nejudne@gmail.com</a>
     </div>
   );
